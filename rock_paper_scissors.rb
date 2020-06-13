835cb946fabfb3d6fda2b67ae067d1d6ad29a9cc
Robert De Matteo <robertdematteo@hotmail.com>
2020-06-14 09:59:39 +1000

    added name option for player 1

--- rock_paper_scissors.rb
+++ rock_paper_scissors.rb
@@ -6,20 +6,28 @@
 class Player
   MOVES = %i[rock paper scissors].freeze
 
-  attr_reader :score, :move
+  attr_reader :score, :move, :name
+  attr_writer :name
+
 
   def initialize
     @score = 0
     @move = nil
+    @name = 'Player 1'
   end
 
   def get_move
     loop do
       puts 'Pick a move'
       print '> '
-      @move = gets.chomp.strip.downcase.to_sym #convert string to symbol eg :rock
-      return @move if MOVES.include?(@move)
-      puts "Invalid move. Try Rock, Paper or Scissors"
+      @move = gets.chomp.strip.downcase.to_sym # convert string to symbol eg :rock
+      if @move == :quit
+        return false
+      elsif MOVES.include?(@move)
+        return @move
+      else
+        puts 'Invalid move. Try Rock, Paper or Scissors'
+      end
     end
   end
 
@@ -30,10 +38,19 @@ end
 
 class ComputerPlayer < Player
 
+  def initialize
+    super
+    @name = 'Computer'
+    
+  end
+
+  def name
+    @name
+  end
+
   def get_move
     @move = Player::MOVES.sample
   end
-
 end
 
 class Game
@@ -45,48 +62,56 @@ class Game
 
   def initialize
     @p1 = Player.new
+    puts " What is your name"
+    @p1.name = gets.chomp.strip
+    puts "Welcome #{@p1.name}"
     @p2 = ComputerPlayer.new
     # @p2 = Player.new
-    
-  
+    play
   end
-
-
 
   def pick_winner
     if @p1.move == @p2.move
       puts "It's a tie"
     elsif WIN_SCENARIOS.include?([@p1.move, @p2.move])
-      puts 'P1 wins'
+      puts "#{@p1.name} wins"
       @p1.win
     else
-      puts 'p2 wins'
+      puts "#{@p2.name} wins"
       @p2.win
-      p @p2
     end
   end
 
   def play_round
-    @p1.get_move
-    @p2.get_move
+    return false unless @p1.get_move
+    return false unless @p2.get_move
     print_moves
     pick_winner
     print_scores
+    puts
+    puts "Lets play again? Type 'quit' to end"
+    true
   end
-  
+
+  def play
+    loop do
+      break unless play_round
+    end
+    puts 'Thanks for playing'
+    print_scores
+  end
+
   def print_moves
-    puts "p1 played #{@p1.move}, p2 played #{@p2.move}"
+    puts "#{@p1.name} played #{@p1.move}, #{@p2.name} played #{@p2.move}"
   end
 
   def print_scores
-    puts "Scores"
-    puts "p1:#{@p1.score}"
-    puts "p2:#{@p2.score}"
+    puts
+    puts 'Scores'
+    puts
+    puts "#{@p1.name}: #{@p1.score}"
+    puts "#{@p2.name}: #{@p2.score}"
   end
 end
 
-game = Game.new
-# game.pick_winner(%i[paper rock])
-# p Player.new.get_move
-
-game.play_round
+Game.new
